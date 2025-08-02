8b48b93f018ca5fbeee7caf2e4ec1f2ff2f3e7cb
annassalam <152462742+annassalam@users.noreply.github.com>
2025-08-02 16:28:04 +0700

    Update register.js

--- api/register.js
+++ api/register.js
@@ -1,23 +1,22 @@
 export default async function handler(req, res) {
   if (req.method !== "POST") {
-    return res.status(405).json({ error: "Method Not Allowed" });
+    return res.status(450).json({ error: "Method Not Allowed"});
   }
 
   try {
     const response = await fetch("https://script.google.com/macros/s/AKfycbya4lK9TqpmV-lhKEDiG03eJIi5Ce5dSh5blR08NCeB9jAjZTk_t0JyX1Qew-CVLWkgjA/exec", {
       method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
+      headers: { "Content-Type": "application/json"},
       body: JSON.stringify(req.body)
     });
 
     const result = await response.json();
     res.status(200).json(result);
   } catch (error) {
-    res.status(500).json({
-      error: "Gagal menghubungi Google Apps Script",
-      message: error.message
+    res.status.(500).json({
+      status: "error",
+      message: "Gagal menghubungi server registrasi",
+      detail: error.message
     });
   }
 }
